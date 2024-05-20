5953b23140908adab6a21a431d9d929805e75412
ZhouyueZhu <zhouyue.23@intl.zju.edu.cn>
2024-05-20 20:06:07 +0800

    Add Practical7

--- Practical7/dalys.py
+++ Practical7/dalys.py
@@ -15,6 +15,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-dayls_days = pd.read_csv("./dalys-rate-from-all-causes.csv")
-dayls_days.head(5)
-print(dayls_days)
+dalys_raw = pd.read_csv("./dalys-rate-from-all-causes.csv")
+
+# 1-100, every 10 rows
+print(dalys_raw.iloc[0:101:10,3])
+
+# DALYs of Afghanistan
+dalys_afghanistan = dalys_raw.loc[dalys_raw["Entity"] == "Afghanistan", "DALYs"]
+print(dalys_afghanistan)
+
+# DALYs of China
+dalys_china = dalys_raw.loc[dalys_raw["Entity"] == "China", ["Entity","Year","DALYs"]]
+print(dalys_china)
+# Calculate the mean value
+china_mean_dalys = dalys_china["DALYs"].mean()
+print(f"The mean DALYs of China is {china_mean_dalys}")
+# DALYs in China in 2019 is less than the mean
+
+# Draw the plot
+plt.plot(dalys_china.Year, dalys_china.DALYs)
+plt.title("DALYs of China")
+plt.show()
+
+# Additional Question
+dalys_uk = dalys_raw.loc[dalys_raw["Entity"] == "United Kingdom", ["Entity","Year","DALYs"]]
+print(dalys_uk)
+
+# Draw the plot
+plt.plot(dalys_uk.Year, dalys_uk.DALYs)
+plt.title("DALYs of UK")
+plt.show()
